30a3ae2ffb4cc2c2e5827a6f3b34aa08b065d6fd
mementum <mementum@users.noreply.github.com>
2015-11-01 00:29:08 +0100

    Tests for the extended retrieval api added

--- tests/test_params_api.py
+++ tests/test_params_api.py
@@ -81,6 +81,79 @@ def test_run(main=False):
     assert p.p3 == p._default('p3')
     assert p._doc('p3') == 'None here'
 
+    # Check list retrieval api
+    p = PX(dict(p2=88))
+
+    names = p._names()
+    assert len(names) == 3
+    assert names[0] == 'p1'
+    assert names[1] == 'p2'
+    assert names[2] == 'p3'
+
+    values = p._values()
+    assert len(values) == 3
+    assert values[0] == p.p1
+    assert values[1] == p.p2
+    assert values[2] == p.p3
+
+    values = p._values('p2', 'p3')
+    assert len(values) == 2
+    assert values[0] == p.p2
+    assert values[1] == p.p3
+
+    defs = p._defaults()
+    assert len(defs) == 3
+    assert defs[0] == p._default('p1')
+    assert defs[1] == p._default('p2')
+    assert defs[2] == p._default('p3')
+
+    defs = p._defaults('p2', 'p3')
+    assert len(defs) == 2
+    assert defs[0] == p._default('p2')
+    assert defs[1] == p._default('p3')
+
+    docs = p._docs()
+    assert len(docs) == 3
+    assert docs[0] == p._doc('p1')
+    assert docs[1] == p._doc('p2')
+    assert docs[2] == p._doc('p3')
+
+    docs = p._docs('p2', 'p3')
+    assert len(docs) == 2
+    assert docs[0] == p._doc('p2')
+    assert docs[1] == p._doc('p3')
+
+    # Check kw retrieval api
+    kwvalues = p._kwvalues()
+    assert len(kwvalues) == 3
+    kvalues = [(x, getattr(p, x)) for x in p._names()]
+    assert list(kwvalues.items()) == kvalues
+
+    kwvalues = p._kwvalues('p2', 'p3')
+    assert len(kwvalues) == 2
+    kvalues = [(x, getattr(p, x)) for x in p._names()[1:]]
+    assert list(kwvalues.items()) == kvalues
+
+    kwdefaults = p._kwdefaults()
+    assert len(kwdefaults) == 3
+    kdefaults = [(x, p._default(x)) for x in p._names()]
+    assert list(kwdefaults.items()) == kdefaults
+
+    kwdefaults = p._kwdefaults('p2', 'p3')
+    assert len(kwdefaults) == 2
+    kdefaults = [(x, p._default(x)) for x in p._names()[1:]]
+    assert list(kwdefaults.items()) == kdefaults
+
+    kwdocs = p._kwdocs()
+    assert len(kwdocs) == 3
+    kdocs = [(x, p._doc(x)) for x in p._names()]
+    assert list(kwdocs.items()) == kdocs
+
+    kwdocs = p._kwdocs('p2', 'p3')
+    assert len(kwdocs) == 2
+    kdocs = [(x, p._doc(x)) for x in p._names()[1:]]
+    assert list(kwdocs.items()) == kdocs
+
 
 if __name__ == '__main__':
     test_run(main=True)
