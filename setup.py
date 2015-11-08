96a46e71cca3d094e9824ae82e82a256af418aa3
mementum <mementum@users.noreply.github.com>
2015-11-08 12:47:41 +0100

    pip packaging fixes for Py3

--- setup.py
+++ setup.py
@@ -31,11 +31,16 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+# Package name
 pname = 'metaparams'
-execfile(os.path.join(pname, 'version.py'))
-pversion = __version__
+
+# Get the version ... execfile is only on Py2 ... use exec + compile + open
+vname = 'version.py'
+with open(os.path.join(pname, vname)) as f:
+    exec(compile(f.read(), vname, 'exec'))
+
 gurl = 'https://github.com/mementum/' + pname
-gdurl = gurl + '/tarball/' + pversion
+gdurl = gurl + '/tarball/' + __version__
 
 setuptools.setup(
     name=pname,
@@ -43,7 +48,7 @@ setuptools.setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version=pversion,
+    version=__version__,
 
     description=('Declarative parameters definition for classes'),
 
